83da936628ce920ad4fb3e35a6985344d479f647
Nicholas Rodgers <nick.rodgers42@gmail.com>
2018-07-29 23:44:51 +0200

    add People page

--- imageResizer.py
+++ imageResizer.py
@@ -6,27 +6,29 @@ if __name__ == "__main__":
         print(i)
         for j in os.listdir("originalImages/" + i):
             # print ("\t-" + j)
-            imageFile = "originalImages/" + i + "/" + j
-            print(imageFile)
-            # try:
-            originalImg = Image.open(imageFile)
-            print(originalImg)
-            print(originalImg.size)
-            width = originalImg.size[0]
-            height = originalImg.size[1]
-            wider = True if (width > height) else False
-            print(height)
-            if wider:
-                newWidth = 700
-                newHeight = newWidth * (height / width)
-            else:
-                newHeight = 700
-                newWidth = newHeight * (width / height)
-            smallerImg = originalImg.resize((int(newWidth), int(newHeight)), Image.ANTIALIAS)
-            if not os.path.exists("images/"):
-                os.makedirs("images/")
-            if not os.path.exists("images/" + i):
-                os.makedirs("images/" + i)
-            smallerImg.save("images/" + i + "/" + j)
+            fileExtension = j.split('.')[1]
+            if fileExtension != "gif":
+                imageFile = "originalImages/" + i + "/" + j
+                print(imageFile)
+                # try:
+                originalImg = Image.open(imageFile)
+                print(originalImg)
+                print(originalImg.size)
+                width = originalImg.size[0]
+                height = originalImg.size[1]
+                wider = True if (width > height) else False
+                print(height)
+                if wider:
+                    newWidth = 700
+                    newHeight = newWidth * (height / width)
+                else:
+                    newHeight = 700
+                    newWidth = newHeight * (width / height)
+                smallerImg = originalImg.resize((int(newWidth), int(newHeight)), Image.ANTIALIAS)
+                if not os.path.exists("images/"):
+                    os.makedirs("images/")
+                if not os.path.exists("images/" + i):
+                    os.makedirs("images/" + i)
+                smallerImg.save("images/" + i + "/" + j)
             # except:
                 # print("Can't load image")
--- photoCarouselMaker.py
+++ photoCarouselMaker.py
@@ -3,15 +3,15 @@ import os
 if __name__ == "__main__":
     goTo = "people"
     print('<el-col :span = "12" > \
-    \n    <h1 v-if = "lang == \'english\'" v-on: click = "goToPage(\'' + goTo + '\')" >People</h1 > \
-    \n    <h1 v-else v-on: click = "goToPage(\'' + goTo + '\')" >Las Personas</h1 > \
+    \n    <h1 v-if = "lang == \'english\'" v-on:click = "goToPage(\'' + goTo + '\')" >People</h1 > \
+    \n    <h1 v-else v-on:click = "goToPage(\'' + goTo + '\')">Las Personas</h1 > \
     \n    <el-carousel indicator-position = "outside" >')
 
     photoDir = "originalImages/people/"
     for i in os.listdir(photoDir):
         # print(i)
-        print('        <el-carousel-item > \
+        print('        <el-carousel-item> \
         \n            <img src = "' + photoDir + i + '" > \
-        \n        </el-carousel-item >')
+        \n        </el-carousel-item>')
     print('     </el-carousel > \
         \n</el-col>')
